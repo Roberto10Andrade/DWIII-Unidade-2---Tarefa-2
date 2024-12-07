988fa2e55ab9074483853b7f64b8b97e7449b419
Roberto Andrade <120339648+Roberto10Andrade@users.noreply.github.com>
2024-12-07 20:00:12 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="teste">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Teste",
+};
+</script>
+
+<style>
+#teste {
+  text-align: center;
+  font-family: Arial;
+  color: #037bf4;
+}
+</style>
